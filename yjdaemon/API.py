1758d5727c8ff7d15cd070c0a64addb05c73ec34
Yorick Rommers <yorick-rommers@hotmail.com>
2015-10-12 01:12:31 +0200

    Fixed error in getsongbyid

--- yjdaemon/API.py
+++ yjdaemon/API.py
@@ -1,8 +1,7 @@
 import json
 
 from yjdaemon.Database import Database as db
-from yjdaemon.maindaemon import MUSIC_DIR
-
+import configparser
 """
 Add a key to the validAPIcalls dictionary, with a corresponding function
 Function should return jsonified data, so that it can then be passed on to the client.
@@ -49,7 +48,7 @@ class calls:
 
     @staticmethod
     def getartists(args):
-        return calls.jsonify({"artists": db.executequerystatic("SELECT artistName FROM tracks GROUP BY artistName;"), "args": args})   @staticmethod
+        return calls.jsonify({"artists": db.executequerystatic("SELECT artistName FROM tracks GROUP BY artistName;"), "args": args})
 
     @staticmethod
     def getalbums(args):
@@ -69,16 +68,23 @@ class calls:
 
     @staticmethod
     def getsongbyid(args):
+        config = configparser.ConfigParser()
+        try:
+            config.read("config.cfg")
+            musicdir = config.get("Library","musicdir")
+            port = config.get("HTTP","port")
+        except:
+            return calls.jsonify({"result" : "NOK" , "errormsg" : "I/O error while reading config."})
         data = args.split("&")
         splitsting = data[0].split("=")
         id = splitsting[1]
         file = db.executequerystatic(
-            "SELECT SUBSTRING_INDEX(trackUrl,'" + MUSIC_DIR + "',-1) as filedir FROM `tracks` WHERE id = " + id)
+            "SELECT SUBSTRING_INDEX(trackUrl,'" + musicdir + "',-1) as filedir FROM `tracks` WHERE id = " + id)
         try:
             url = str(file[0][0])
         except:
             return calls.jsonify({"result": "NOK", "errormsg" : "Song ID does not exist in database."})
-        return calls.jsonify({"result": "OK", "songurl": "http://localhost:8585/" + url})
+        return calls.jsonify({"result": "OK", "songurl": "http://localhost:"+ port + url})
 
     @staticmethod
     def setsong(args, songname):
